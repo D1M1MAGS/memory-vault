6cf46f2d058131f0f70bf2dff546b4a16f601878
D1M1MAGS <53001378+D1M1MAGS@users.noreply.github.com>
2020-02-11 19:44:44 +1100

    turnover and db switch

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBWpIq48WxJvAWDSTEca2qQnt_umVG6ckQ",
+    authDomain: "fir-login-photovault.firebaseapp.com",
+    databaseURL: "https://fir-login-photovault.firebaseio.com",
+    projectId: "fir-login-photovault",
+    storageBucket: "fir-login-photovault.appspot.com",
+    messagingSenderId: "1036864759904",
+    appId: "1:1036864759904:web:d91873a3e3d2fc6947bb05",
+    measurementId: "G-1PPQHJBLY6"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
