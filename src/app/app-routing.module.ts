2048ea06163d1258c092df9f4e6fef66e1d16184
D1M1MAGS <53001378+D1M1MAGS@users.noreply.github.com>
2020-02-11 20:16:26 +1100

    google maps page generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,6 +38,10 @@ const routes: Routes = [
   {
     path: 'checklist',
     loadChildren: () => import('./checklist/checklist.module').then( m => m.ChecklistPageModule)
+  },
+  {
+    path: 'google-maps',
+    loadChildren: () => import('./google-maps/google-maps.module').then( m => m.GoogleMapsPageModule)
   }
 
 ];
